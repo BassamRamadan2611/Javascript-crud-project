a2aa89c71442c286eaa7a03f2254cdbd243ebc30
Bassam Ramadan <78547180+BassamRamadan2611@users.noreply.github.com>
2023-11-03 19:47:29 +0200

    Update first.js

--- js/first.js
+++ js/first.js
@@ -1,6 +1,14 @@
 
+/// declaring general variables
+
 var productsContainer;
+var myIndex ;
+var addproduct = true 
 var inps=document.getElementsByClassName("form-control");
+var productName = document.getElementById("productNameInp")
+var productPrice = document.getElementById("productPriceInp")
+var productCategory = document.getElementById("productCategoryInp")
+var productDesc = document.getElementById("productDescInp")
 
 if(localStorage.getItem("productsData")== null){
   productsContainer=[];
@@ -10,43 +18,35 @@ else{
   displayData();
 
 }
+hideEdit()
 
+/// function add products to productscontainer array 
 function addProuduct() {
-
-
-
+  
 var productName = document.getElementById("productNameInp").value;
 var productPrice = document.getElementById("productPriceInp").value;
 var productCategory = document.getElementById("productCategoryInp").value;
 var productDesc = document.getElementById("productDescInp").value;
 
-
-
-
 var product= {
+
   name: productName,
   price:productPrice,
   category:productCategory ,
   desc:productDesc,
 
-
-
-
 }
-
-
 productsContainer.push(product);
-
-
+// save data in locall storage 
 localStorage.setItem("productsData",JSON.stringify(productsContainer));
 displayData();
 
 clearForm();
  
-
- 
 }
 
+
+// function to display data in boxes 
 function displayData(){
 
   var temp="";
@@ -62,20 +62,22 @@ for (var i=0; i<productsContainer.length;i++){
 </div>
   </div>
   <button onclick="delateData(`+i+`)" class="btn btn-outline-danger mb-3 "> Delate</button>
-  <button onclick="update()" class="btn btn-outline-warning mb-3">Update</button>
+  <button onclick="editMode(`+i+`)" class="btn btn-outline-warning mb-3">Update</button>
 
 </div>`
 }
 document.getElementById("rowBody").innerHTML=temp;
 
-
-
 }
+
+// function to clear form data after add product 
 function clearForm(){
   for(var i=0; i<inps.length;i++){
     inps[i].value = "";
   }
 }
+
+// function to delte selected product 
 function delateData(index){
 
   var delated = productsContainer.splice(index,1);
@@ -83,9 +85,9 @@ function delateData(index){
 localStorage.setItem("productsData",JSON.stringify(productsContainer));
 displayData();
   
-  
 
 }
+// function to search about specific product 
   function searchProducts(term){
     var temp="";
     for(var i=0;i<productsContainer.length ;i++){
@@ -103,7 +105,7 @@ displayData();
       </div>
         </div>
         <button onclick="delateData(`+i+`)" class="btn btn-outline-danger mb-3 "> Delate</button>
-        <button  onclick="update()" class="btn btn-outline-warning mb-3">Update</button>
+        <button  onclick="editMode(`+i+`)" class="btn btn-outline-warning mb-3">Update</button>
    
       </div>`
     }
@@ -111,6 +113,8 @@ displayData();
   }
 
   }
+
+  // function to set valadation on input name 
 
   function validateName(userName){
 
@@ -127,6 +131,7 @@ displayData();
       document.getElementById("addBtn").removeAttribute("disabled");
     }
   }
+    // function to set valadation on input price 
   function validatePrice(userName){
 
     var userNameRegx =/^[1-9][A-Z]?/;
@@ -141,9 +146,57 @@ displayData();
       document.getElementById("addBtn").removeAttribute("disabled");
     }
   }
-  function update(){
- 
- 
-  
-     
+
+// function to edit product 
+function editProduct(){
+
+  var productName = document.getElementById("productNameInp").value;
+  var productPrice = document.getElementById("productPriceInp").value;
+  var productCategory = document.getElementById("productCategoryInp").value;
+  var productDesc = document.getElementById("productDescInp").value;
+
+ let updatedProduct  = {
+  name: productName,
+  price:productPrice,
+  category:productCategory ,
+  desc:productDesc,
+
+}
+productsContainer[myIndex] = updatedProduct
+
+ localStorage.setItem("productsData",JSON.stringify(productsContainer));
+ console.log("product updated " + updatedProduct)
+clearForm()
+displayData()
+
+}
+
+  function  editMode(index) {
+    showEdit()
+    myIndex = index
+  document.getElementById("productNameInp").value =  productsContainer[index].name
+  document.getElementById("productPriceInp").value =  productsContainer[index].price
+  document.getElementById("productCategoryInp").value = productsContainer[index].category
+  document.getElementById("productDescInp").value = productsContainer[index].desc
+
+  }
+
+  // get data of products from local storage 
+
+  (() => {
+
+    data = JSON.parse(localStorage.getItem("productsData")) || []
+     console.log(data)
+    
+  })();
+
+
+
+
+   function hideEdit(){
+    document.getElementById('edit').style.display ='none'
+    
+  }
+  function showEdit() {
+    document.getElementById('edit').style.display ='inline'
   }
